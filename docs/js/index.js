31548ea3a723059c5b35380ac4e7261b40ddc173
Thomas Otterson <barandis1729@gmail.com>
2020-12-08 02:17:56 -0500

    change from promise-based to generator-based algorithms

--- docs/js/index.js
+++ docs/js/index.js
@@ -55,82 +55,21 @@ function initCanvas() {
 
   ctx.clearRect(0, 0, width, height)
   ctx.translate(width / 2, height / 2)
+  ctx.rotate(-Math.PI / 2)
 
   radius = Math.min(height, width) * 0.95 / 2
 }
 
-function initArray(array, ppf = 10) {
-  let i = 0
+function execute(genFn, array, epf ) {
+  const gen = genFn(array, epf)
 
   return new Promise(resolve => {
     requestAnimationFrame(function update(time) {
-      for (let j = 0; j < ppf; j++) {
-        array[i] = i++
-        if (i >= elementCount) break
-      }
+      const value = gen.next()
       draw(array)
 
-      if (i < elementCount) {
-        requestAnimationFrame(update)
-      } else {
-        resolve()
-      }
-    })
-  })
-}
-
-function shuffle(array, ppf = 10) {
-  let i = array.length - 1
-
-  return new Promise(resolve => {
-    requestAnimationFrame(function update(time) {
-      for (let x = 0; x < ppf; x++) {
-        const j = Math.floor(Math.random() * (i + 1))
-        const temp = array[i]
-        array[i] = array[j]
-        array[j] = temp
-        i--
-        if (i <= 0) break
-      }
-      draw(array)
-
-      if (i > 0) {
-        requestAnimationFrame(update)
-      } else {
-        resolve()
-      }
-    })
-  })
-}
-
-function bubbleSort(array, ppf = 10) {
-  let len = array.length
-  let i = 0
-  let j = 0
-  let noswaps = true
-  let running = true
-
-  return new Promise(resolve => {
-    requestAnimationFrame(function update(time) {
-      for (let x = 0; x < ppf; x++) {
-        if (array[j] > array[j + 1]) {
-          [array[j + 1], array[j]] = [array[j], array[j + 1]]
-          noswaps = false
-        }
-        j++
-
-        if (j >= len) {
-          if (noswaps) running = false
-          noswaps = true
-          j = 0
-          i++
-        }
-        if (i >= len) break
-      }
-      draw(array)
-
-      if (!running || i >= len) {
-        resolve()
+      if (value.done) { 
+        resolve() 
       } else {
         requestAnimationFrame(update)
       }
@@ -138,168 +77,178 @@ function bubbleSort(array, ppf = 10) {
   })
 }
 
-function insertionSort(array, ppf = 10) {
-  let i = 1
-  let j = 0
+function *initArray(array, epf = 10) {
+  let e = 0
+  for (let i = 0; i < elementCount; i++) {
+    array[i] = i
 
-  return new Promise(resolve => {
-    requestAnimationFrame(function update(time) {
-      for (x = 0; x < ppf; x++) {
-        if (array[j + 1] < array[j]) {
-          [array[j + 1], array[j]] = [array[j], array[j + 1]]
-        }
-        j--
-
-        if (j <= -1) {
-          i++
-          if (i >= array.length) break
-          j = i - 1
-        }
-      }
-      draw(array)
-
-      if (i < array.length) {
-        requestAnimationFrame(update)
-      } else {
-        resolve()
-      }
-    })
-  })
+    if (++e >= epf) {
+      e = 0
+      yield i
+    }
+  }
 }
 
-function selectionSort(array, ppf = 10) {
-  let i = 0
-  let j = 1
-  let min = 0
+function *shuffle(array, epf = 10) {
+  let e = 0
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
 
-  return new Promise(resolve => {
-    requestAnimationFrame(function update(time) {
-      for (let x = 0; x < ppf; x++) {
-        if (array[j] < array[min]) min = j
-        j++
-
-        if (j >= array.length) {
-          if (min !== i) {
-            [array[i], array[min]] = [array[min], array[i]]
-          }
-          i++
-
-          if (i >= array.length) break
-          min = i
-          j = i + 1
-        }
-      }
-      draw(array)
-
-      if (i < array.length) {
-        requestAnimationFrame(update)
-      } else {
-        resolve()
-      }
-    })
-  })
+    if (++e >= epf) {
+      e = 0
+      yield i
+    }
+  }
 }
 
-function merge(array, start, mid, end, ppf) {
+function *bubbleSort(array, epf = 10) {
+  const len = array.length
+  let e = 0
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]]
+      }
+
+      if (++e >= epf) {
+        e = 0
+        yield i
+      }
+    }
+  }
+}
+
+function *insertionSort(array, epf = 10) {
+  const len = array.length
+  let e = 0
+
+  for (let i = 0; i < len; i++) {
+    const key = array[i]
+    let j = i - 1
+
+    while (j >= 0 && key < array[j]) {
+      array[j + 1] = array[j]
+      j -= 1
+
+      if (++e >= epf) {
+        e = 0
+        yield i
+      }
+    }
+    array[j + 1] = key
+  }
+}
+
+function *selectionSort(array, epf = 10) {
+  const len = array.length
+  let e = 0
+
+  for (let i = 0; i < len; i++) {
+    let min = i
+    for (let j = i + 1; j < len; j++) {
+      if (array[min] > array[j]) min = j
+
+      if (++e >= epf) {
+        e = 0
+        yield i
+      }
+    }
+    [array[i], array[min]] = [array[min], array[i]]
+  }
+}
+
+function *merge(array, start, mid, end, epf) {
+  let e = 0
   let start2 = mid + 1
 
-  return new Promise(resolve => {
-    if (array[mid] <= array[start2]) {
-      resolve()
+  if (array[mid] <= array[start2]) return
+
+  while (start<= mid && start2 <= end) {
+    if (array[start] <= array[start2]) {
+      start++
     } else {
-      requestAnimationFrame(function update(time) {
-        for (let x = 0; x < ppf; x++) {
-          if (start > mid || start2 > end) break
+      const value = array[start2]
+      let i = start2
 
-          if (array[start] <= array[start2]) {
-            start++
-          } else {
-            const value = array[start2]
-            let i = start2
+      while (i !== start) {
+        array[i] = array[i - 1]
+        i--
+      }
 
-            while (i !== start) {
-              array[i] = array[i - 1]
-              i--
-            }
-            array[start] = value
+      if (++e >= epf) {
+        e = 0
+        yield i
+      }
 
-            start++
-            mid++
-            start2++
-          }
-        }
-        draw(array)
+      array[start] = value
 
-        if (start > mid || start2 > end) {
-          resolve()
-        } else {
-          requestAnimationFrame(update)
-        }
-      })
+      if (++e >= epf) {
+        e = 0
+        yield i
+      }
+
+      start++
+      mid++
+      start2++
     }
-  })
+  }
 }
 
-function mergeSort(array, ppf = 10, left = 0, right = array.length - 1) {
+function *mergeSort(array, epf = 10, left = 0, right = array.length - 1) {
   if (left < right) {
     const mid = left + Math.floor((right - left) / 2)
-    return mergeSort(array, ppf, left, mid)
-      .then(() => mergeSort(array, ppf, mid + 1, right))
-      .then(() => merge(array, left, mid, right, ppf))
+
+    yield *mergeSort(array, epf, left, mid)
+    yield *mergeSort(array, epf, mid + 1, right)
+
+    yield *merge(array, left, mid, right, epf)
   }
-  return Promise.resolve()
 }
 
-function partition(array, start, end, ppf) {
-  let pivot = array[start]
-  let swap = start
-  let i = start + 1
+function *partition(array, left, right, epf) {
+  let pivot = array[right]
+  let i = left - 1
+  let e = 0
 
-  return new Promise(resolve => {
-    requestAnimationFrame(function update(time) {
-      for (let x = 0; x < ppf; x++) {
-        if (array[i] < pivot) {
-          swap++
-          [array[i], array[swap]] = [array[swap], array[i]]
-        }
-        i++
-
-        if (i > end) break
-      }
-      draw(array)
-  
-      if (i <= end) {
-        requestAnimationFrame(update)
-      } else {
-        [array[swap], array[start]] = [array[start], array[swap]]
-        resolve(swap)
-      }
-    })
-  })
+  for (let j = left; j < right; j++) {
+    if (array[j] < pivot) {
+      i++
+      [array[i], array[j]] = [array[j], array[i]]
+    }
+    if (++e >= epf) {
+      e = 0
+      yield i
+    }
+  }
+  [array[i + 1], array[right]] = [array[right], array[i + 1]]
+  return i + 1
 }
 
-function quickSort(array, ppf = 10, left = 0, right = array.length - 1) {
+function *quickSort(array, epf = 10, left = 0, right = array.length - 1) {
   if (left < right) {
-    const pa = partition(array, left, right, ppf)
-    const pb = pa.then(pivot => quickSort(array, ppf, left, pivot - 1))
-    return Promise.all([pa, pb]).then(([pivot, _]) => quickSort(array, ppf, pivot + 1, right))
+    const i = yield *partition(array, left, right, epf)
+    yield *quickSort(array, epf, left, i - 1)
+    yield *quickSort(array, epf, i + 1, right)
   }
-  return Promise.resolve()
 }
 
 initCanvas()
-initArray(array, 10)
-  .then(() => shuffle(array, 10))
-  .then(() => bubbleSort(array, 1500))
+execute(initArray, array, 10)
+  .then(() => execute(shuffle, array, 10))
+  .then(() => execute(bubbleSort, array, 1500))
   .then(() => pause(2000))
-  .then(() => shuffle(array, 10))
-  .then(() => insertionSort(array, 1000))
+  .then(() => execute(shuffle, array, 10))
+  .then(() => execute(insertionSort, array, 750))
   .then(() => pause(2000))
-  .then(() => shuffle(array, 10))
-  .then(() => selectionSort(array, 1000))
+  .then(() => execute(shuffle, array, 10))
+  .then(() => execute(selectionSort, array, 1250))
   .then(() => pause(2000))
-  .then(() => shuffle(array, 10))
-  .then(() => mergeSort(array, 10))
+  .then(() => execute(shuffle, array, 10))
+  .then(() => execute(mergeSort, array, 10))
   .then(() => pause(2000))
-  .then(() => shuffle(array, 10))
-  .then(() => quickSort(array, 10))
+  .then(() => execute(shuffle, array, 10))
+  .then(() => execute(quickSort, array, 10))
